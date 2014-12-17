1dfdc51ebe2f05ffe6ca01eb58457127b54b1c43
Flavia Goncalves <fgoncalves@prosper.com>
2014-12-16 16:47:31 -0800

    commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('todoApp', [
+  'ui.router',
+  'templates-app',
+  'ngAria',
+  'angularMoment',
+  'ngSanitize'
+])
+
+    .controller("ToDoController", function ToDoController(){
+      var toDoCtrl = this;
+
+      var model = {
+        user: "Adam",
+        items: [{ action: "Buy Flowers", done: false },
+          { action: "Get Shoes", done: false },
+          { action: "Collect Tickets", done: true },
+          { action: "Call Joe", done: false }]
+      };
+
+      toDoCtrl.todo = model;
+    })
+
+;
